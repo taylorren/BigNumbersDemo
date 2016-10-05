f2832e61d2ad55b18e186cc689322cba758a67c7
taylorren <taylor.ren@gmail.com>
2016-10-05 11:38:16 +0800

    Update rsa.php

--- rsa.php
+++ rsa.php
@@ -1,35 +1,25 @@
 <?php
 
-$letter = 'A';
+$letters = ['W','e','l','c','o','m','e'];
 
-$m = ord($letter); // ASCII code of letter 'A'
-$d = 2753;
-
-$e = 17;
-$n = 3233;
-
-echo "Encryption / Decryption using BC:\n";
-$c = bcmod(bcpow($m, $e, 40), $n);
-$res = bcmod(bcpow($c, $d, 40), $n);
-
-echo "Original data: $letter\n";
-echo "Encrypted data: $c\n";
-echo "Decryption:\n";
-echo "Input: $c\n";
-echo "Decrypted: $res\n";
-echo "The letter is: ".chr($res)."\n";
-
-echo "=========================\n";
-echo "Encryption / Decryption using GMP:\n";
-$c = gmp_powm($m, $e, $n);
-$res = gmp_powm($c, $d, $n);
-
-echo "Original data: $letter\n";
-echo "Encrypted data:".gmp_strval($c)." \n";
-echo "Decryption:\n";
-echo "Input: ".gmp_strval($c)."\n";
-echo "Decrypted: ".gmp_strval($res)."\n";
-echo "The letter is: ".chr(gmp_strval($res))."\n";
+foreach($letters as $letter)
+{
+	$p1=881;
+	$p2=883;
+	$n = $p1*$p2;	
+	$e = 17; // must coprimes with (p1-1)*(p2-1)=phi(n)
+	$d = 91313; // d*e = 1 mod (phi(n))
+	
 
 
+	echo $letter."\t";
 
+	$m = ord($letter); // ASCII code of letter 'A'
+
+	$c = bcmod(bcpow($m, $e, 0), $n);
+	$res = bcmod(bcpow($c, $d, 0), $n);
+
+	echo "$c\t";
+	echo "Decrypted: $res\t";
+	echo "The letter is: ".chr($res)."\n";
+}
